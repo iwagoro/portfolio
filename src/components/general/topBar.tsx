c697d15e17dffd7bfdd380a27bc55db3d05aaf02
rockwell <iwagoro733@gmail.com>
2024-01-08 19:47:32 +0900

    added icon

--- src/components/general/topBar.tsx
+++ src/components/general/topBar.tsx
@@ -7,8 +7,8 @@ export default function TopBar() {
     return (
         <div className="w-screen h-[100px] box-border fixed top-5 left-0 flex justify-center items-center z-50">
             <div className="w-screen max-w-7xl flex items-center justify-between">
-                <Link href="/" className="flex">
-                    <img src="./logo.svg" className="ml-10 w-[32px] h-[32px]  " />
+                <Link href="/" className="flex items-center">
+                    <img src="./logo.svg" className="ml-10 logo2  " />
                     <p>ockwell</p>
                 </Link>
                 <div className="mr-10">
